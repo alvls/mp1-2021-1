c13931d5696ecf3bea7fb641da9bd5d320fa3167
AlexanderOfficial <a.krainev02@gmail.com>
2021-02-22 15:02:34 +0300

    task1

---
+++ Krainev_AA/task1/Source.cpp
@@ -0,0 +1,174 @@
+﻿/*Разработать класс Конвертер весов.
+Класс должен хранить вес в килограммах и предоставлять методы по его преобразованию в другие единицы измерения(например, аптечный фунт, тройская унция, пуд, …).
+Класс должен предоставлять операции : 1) установить текущий вес в килограммах, 2) узнать текущий вес в килограммах, 3) узнать текущий вес в выбранной единице измерения(из списка поддерживаемых).
+Класс должен содержать все необходимые конструкторы, оператор присваивания, а также «уметь» выводить себя на консоль.
+*/
+#include <iostream>
+using namespace std;
+
+int getNumberOfWeidth();
+void printMainMenu();
+void printMenuWithNamesOfWeights();
+void infornationAbConverted(int varianOfMenu, class ScaleConverter measuring);
+void welcomeMessage();
+ 
+
+class ScaleConverter {
+private:
+    double enteredWeight; //введенная масса(вес)
+    double convertedWeight; //конвертированная масса(вес)
+public:
+    ScaleConverter(double i); // construct для задания значения веса
+    //методы для конвертации в другие меры веса
+    void kgToTroyOunce();
+    void kgToPharmacyPound();
+    void kgToOunce();//унция обыкновенная
+    void kgToStones();//стоны 
+    void kgToAmericanTones();//американская тонна
+
+    //вывод enteredWeight значения
+    void printOfEnteredWeight();
+    //вывод convertedWeight значения
+    void printOfConvertedWeight();
+};
+int main() {
+    setlocale(LC_ALL, "Rus");
+    welcomeMessage();
+    double kg = -1;
+    while (kg < 0) {
+        cout << "Введите массу в кг. и нажмите ENTER\n" << endl;
+        cin >> kg;
+        if (kg < 0) {
+            cout << "Некорректная величина для конвертации" << endl;
+        }
+    }
+    ScaleConverter measuring(kg);
+    while (1) {
+        int variantOfMenu = -1;
+
+        printMainMenu();
+        while (variantOfMenu <= 0 || variantOfMenu >= 4) {
+            cin >> variantOfMenu;
+            if (variantOfMenu <= 0 || variantOfMenu >= 4) {
+                cout << "Неверный выбор меню, попробуйте снова!" << endl;
+            }
+        }
+
+        switch (variantOfMenu) {
+        case 1: //конвертировать
+            printMenuWithNamesOfWeights();
+            variantOfMenu = getNumberOfWeidth();
+            infornationAbConverted(variantOfMenu, measuring);
+            break;
+        case 2: //просмотр текущего значения (в кг.)
+            measuring.printOfEnteredWeight();
+            break;
+        case 3: //выход из программы
+            //measuring.printOfEnteredWeight();
+            return 0;
+            break;
+        }
+    }
+    return 0;
+}
+
+void welcomeMessage() {
+    cout << "Добро пожаловать в приложение конвертер весов!\n" << endl;
+    cout << "Сначала Вам будет предложено ввести значение для конвертации (в кг.)\n"
+        << "Затем вы сможете выбрать единицу меры веса, в которую хотите провести конвертацию \n"
+        << "Перевод может выведен с некоторой погрешностью, из-за разных значений мер в справочниках\n"
+        << "Значения для перевода взяты с сайта конвертер единиц измерения google.com\n" << endl;
+}
+void printMainMenu() {
+    cout << "Выберите вариант меню:\n" << endl;
+    cout << "1. Конвертировать в другую единицу измерения\n"
+        << "2. Просмотр текущего выбранного значения (в кг.) \n"
+        << "3. Выйти\n" << endl;
+}
+
+void printMenuWithNamesOfWeights() {
+    cout << "Выберите вариант\n" << endl;
+    cout << "1. Конвертация в тройскую унцию \n"
+        << "2. Конвертация в аптечный фунт\n"
+        << "3. Конвертация унцию (обыкновенная) \n"
+        << "4. Конвертация в стоны \n"
+        << "5. Конвертация в американскую тонну\n"
+        << "6. Выйти из программы\n" << endl;
+}
+
+ScaleConverter::ScaleConverter(double i) {
+    enteredWeight = i;
+    convertedWeight = 0;
+}
+
+void ScaleConverter::kgToTroyOunce() {
+    convertedWeight = 32.150743 * enteredWeight;
+    //cout << enteredWeight;
+}
+
+void ScaleConverter::kgToPharmacyPound() {
+    convertedWeight = 2.679229 * enteredWeight;
+}
+
+void ScaleConverter::kgToOunce() {
+    convertedWeight = 35.274 * enteredWeight;
+}
+
+void ScaleConverter::kgToStones() {
+    convertedWeight = 0.157473 * enteredWeight;
+}
+
+void ScaleConverter::kgToAmericanTones() {
+    convertedWeight = 0.00110231 * enteredWeight;
+}
+
+void ScaleConverter::printOfEnteredWeight() {
+    cout << enteredWeight << endl;
+}
+
+void ScaleConverter::printOfConvertedWeight() {
+    cout << convertedWeight << endl;
+}
+
+int getNumberOfWeidth() {
+    int variant = -1;
+    while (variant <= 0 || variant >= 7) {
+        cin >> variant;
+        if (variant <= 0 || variant >= 7) {
+            cout << "Неверный выбор меню, попробуйте снова!" << endl;
+        }
+    }
+    return variant;
+}
+
+void infornationAbConverted(int variantOfMenu, class ScaleConverter measuring) {
+    switch (variantOfMenu) {
+    case 1: //тройская унция
+        cout << " В тройских унциях: " << endl;
+        measuring.kgToTroyOunce();
+        measuring.printOfConvertedWeight();
+        break;
+    case 2: //аптечная унция
+        cout << " В аптечных фунтах: " << endl;
+        measuring.kgToPharmacyPound();
+        measuring.printOfConvertedWeight();
+        break;
+    case 3: //унция обыкновенная
+        cout << " В унциях: " << endl;
+        measuring.kgToOunce();
+        measuring.printOfConvertedWeight();
+        break;
+    case 4: //стоны
+        cout << " В стонах: " << endl;
+        measuring.kgToStones();
+        measuring.printOfConvertedWeight();
+        break;
+    case 5: //американская тонна
+        cout << " В американских тоннах: " << endl;
+        measuring.kgToAmericanTones();
+        measuring.printOfConvertedWeight();
+        break;
+    case 6:
+        exit(1);
+    }
+}
