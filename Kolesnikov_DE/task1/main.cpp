5b1c7352b7b7cc72c0ff1ab73e8d56eb3f54555c
kde37777 <kolessnikoff2016@yandex.ru>
2021-02-28 19:52:50 +0300

    task1 4-th build

--- Kolesnikov_DE/task1/main.cpp
+++ Kolesnikov_DE/task1/main.cpp
@@ -7,63 +7,55 @@ private:
     double YardCoef = 0.9144f;
     double SazhenCoef = 1.8288f;
     double FtCoef = 3.28084f;
-    double len[5] = { 0 };
+    double len;
 public:
-    LenConv(int a) 
+    LenConv(int a)
     {
-        len[0] = a;
+        len = a;
     }
-    void Print(int mode = 0) 
-    {	
-        if (!len[mode]) {
-            if (mode == 0) {
-                cout << "You don`t set init val"<<endl;
-            }
-            else {
-                cout << "You don`t convert to it"<<endl;
-            }
-        }
-        else {
-            cout << len[mode]<<endl;
-        }
-    }
-    void Init(int a) 
+    double Get_Metr() 
     {
-        len[0] = a;
-        for (int i = 1; i < 5; i++) {
-            len[i] = 0;
-        }
+	return len;
+    }
+    void Print()
+    {
+	cout << "Lenght in meters = " << len << endl;
+	cout << "Lenght in Archin = " << ConvArchin() << endl;
+	cout << "Lenght in Yard = " << ConvYard() << endl;
+	cout << "Lenght in Sazhen = " << ConvSazhen() << endl;
+	cout << "Lenght in Ft = " << ConvFt() << endl;
     }
     LenConv& operator= (const LenConv &conv)
     {
-	for(int i = 0;i<5;i++){
-		len[i] = conv.len[i];
-	}
+	len = conv.len;
 	return *this;
     }
-    void Convert(int mode) 
+    double ConvArchin()
     {
-        switch (mode) {
-        case (1):
-            len[mode] = len[0] * ArchinCoef;
-        case (2):
-            len[mode] = len[0] * YardCoef;
-        case (3):
-            len[mode] = len[0] * SazhenCoef;
-        case (4):
-            len[mode] = len[0] * FtCoef;
-        }
+        return len * ArchinCoef;
     }
+    double ConvYard()
+    {
+        return len * YardCoef;
+    }
+    double ConvSazhen() 
+    {
+        return len * SazhenCoef;
+    }
+    double ConvFt()
+    {
+        return len * FtCoef;
+    }
+
 };
 int main()
 {   
     int val = 2;
     LenConv a(val);
-    a.Print();
-    a.Convert(1);
-    a.Print(1);   
+    cout << a.Get_Metr() << endl;
+    cout << a.ConvArchin() << endl;
     LenConv b(3);
     b = a;
-    b.Print(1);
-    b.Print(3);
+    cout << b.Get_Metr() << endl;
+    b.Print();
 }
