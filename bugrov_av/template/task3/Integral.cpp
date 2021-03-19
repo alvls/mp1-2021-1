db8077def231302a7e0591a61d543e3f27a687fc
bugrov.and <bugrovandriy@yandex.ru>
2021-03-19 17:52:36 +0300

    задание 3

--- bugrov_av/template/task3/Integral.cpp
+++ bugrov_av/template/task3/Integral.cpp
@@ -21,23 +21,25 @@ enum way
 	_mid,
 	_right
 };
+double line(double x)
+{
+	return x;
+}
 class integral
 {
-	double a, b;// левый и правый пределы интегрирования
+	double lim[2];// левый и правый пределы интегрирования
 	math foo;// указатель на функцию
 	int n;// число сегментов
 	double res;// результат вычислений
 	way method;// метод интегрирования
-	double midfinder(double a1, double b1)
+	/* double midfinder(double a1, double b1)
 	{
 		return (b1 + (b1 - a1) / 2);
-	}
-public:
+	}*/
 	void calc();// вычисление интеграла
-	integral(double l = 0, double r = 0, math f = cos) :a(l), b(r), foo(f) // конструктор
+public:
+	integral(double l = 0, double r = 0, math f = abs) :lim{ l, r }, foo(f) // конструктор
 	{
-		b = 0;
-		a = 0;
 		n = 1;
 		res = 0;
 		method = _mid;
@@ -48,14 +50,11 @@ public:
 	}
 	void GetLim(double l, double r)// задать пределы интегрирования
 	{
-		a = l;
-		b = r;
+		lim[0] = l;
+		lim[1] = r;
 	}
 	const double* OutLim()// вывод пределов интегрирования
 	{
-		double* lim = new double[2];
-		lim[0] = a;
-		lim[1] = b;
 		return lim;
 	}
 	void GetsegNum(int number)// задать число отрезков метода прямоугольников
@@ -64,6 +63,7 @@ public:
 	}
 	double out() // вывести результат вычисления на экран
 	{
+		calc();
 		return res;
 	}
 	void GetWay(way choice)// выбрать метод вычисления
@@ -73,202 +73,149 @@ public:
 };
 void integral::calc()
 {
-	double addend;
-	double segment = (b - a) / (double)n;
-	double a1 = a;
-	double b1 = a + segment;
-	int i = 0, _n = n;
-	double* x_i = nullptr;
+	res *= 0;
+	double addend, segment = (lim[1] - lim[0]) / n;
+	int  i = 0, _n = n;
+	double x_i = lim[0];
 	switch (method)
 	{
 	case _left:
-		*x_i = a1;
 		break;
 	case _mid:
 		for (; i < _n; i++)
 		{
-			addend = foo(midfinder(a1, b1)) * (b1 - a1);
+			addend = foo(x_i + (i + 0.5) * segment);
 			res += addend;
-			a1 += segment;
-			b1 += segment;
 		}
+		res *= segment;
 		return;
 	case _right:
-		i++;
-		_n++;
-		*x_i = b1;
+		x_i += segment;
 		break;
 	default:
 		cout << "Что-то пошло не так" << endl;
-		break;
+		return;
 	}
 	for (; i < _n; i++)
 	{
-		addend = foo(*x_i) * (b1 - a1);
+		addend = foo(x_i + i * segment);
 		res += addend;
-		a1 += segment;
-		b1 += segment;
 	}
+	res *= segment;
 }
-void printer()
+void usefunk(integral& s)
 {
-
+	enum fname { _exp = 1, _sin, _cos, _sinh, _cosh, _sqrt, _log , _line};
+	cout << "Выберите номер функции:" << endl;
+	cout << "1. Экспонента" << endl;
+	cout << "2. Синус" << endl;
+	cout << "3. Косинус" << endl;
+	cout << "4. Гиперболический синус" << endl;
+	cout << "5. Гиперболический косинус" << endl;
+	cout << "6. Квадратный корень" << endl;
+	cout << "7. Натуральный логарифм" << endl;
+	cout << "8. Прямая y=x" << endl;
+	int ans;
+	cin >> ans;
+	math funk;
+	switch (ans)
+	{
+	case _exp:
+		funk = exp;
+		break;
+	case _sin:
+		funk = sin;
+		break;
+	case _cos:
+		funk = cos;
+		break;
+	case _sinh:
+		funk = sinh;
+		break;
+	case _cosh:
+		funk = cosh;
+		break;
+	case _sqrt:
+		funk = sqrt;
+		break;
+	case _log:
+		funk = log;
+		break;
+	case _line:
+		funk = line;
+		break;
+	default:
+		funk = abs;
+		break;
+	}
+	s.GetFoo(funk);
+}
+void useway(integral& s)
+{
+	int ans;
+	cout << "Выберите метод вычисления интеграла" << endl;
+	cout << "1. Метод левых прямоугольников" << endl;
+	cout << "2. Метод средних прямоугольников"<<endl;
+	cout << "3. Метод правых прямоугольников"<<endl;
+	cin >> ans;
+	way getmeth;
+	switch (ans)
+	{
+	case _left:
+		getmeth = _left;
+		break;
+	case _mid:
+		getmeth = _mid;
+		break;
+	case _right:
+		getmeth = _right;
+		break;
+	default:
+		break;
+	}
+	s.GetWay(getmeth);
+}
+void menu()
+{
+	integral s;
+	const double* limits = nullptr;
+	integral& sum = s;
+	int ans;
+	do {
+		usefunk(sum);
+		cout << "Задайте пределы интегрирования" << endl;
+		double leflim, riglim;
+		bool flag = true;
+		do
+		{
+			cin >> leflim >> riglim;
+			if (leflim < riglim)
+				flag = false;
+			else
+				cout << "Неверные данные" << endl;
+		} while (flag);
+		s.GetLim(leflim, riglim);
+		limits = s.OutLim();
+		cout << "Левый предел интегрирования: " << limits[0] << endl;
+		cout << "Правый предел интегрирования: " << limits[1] << endl;
+		cout << "Задайте число отрезков метода прямоугольников" << endl;
+		int segnum;
+		cin >> segnum;
+		s.GetsegNum(segnum);
+		useway(sum);
+		double res;
+		res = s.out();
+		cout << "Результат вычислений: " << res << endl;
+		char a;
+		cout << "Если вы желаете продолжать, введите 0, иначе введите любой другой символ" << endl;
+		cin >> a;
+		ans = static_cast<int>(a) - 48;
+	} while (!ans);
 }
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	int a;
-	integral s;
-	const double* limits = nullptr;
-	enum fname { _exp = 1, _sin, _cos, _sinh, _cosh, _sqrt, _log };
-	do {
-	cout << "1. Задать текущую функцию" << endl;
-	cout << "2. Задать пределы интегрирования" << endl;
-	cout << "3. Узнать пределы интегрирования" << endl;
-	cout << "4. Задать число отрезков метода прямоугольников" << endl;
-	cout << "5. Выбрать метод вычисления" << endl;
-	cout << "6. Вычислить значение интеграла выбранным методом" << endl; 
-	cout << "7. Вывести результат вычисления на экран" << endl;
-	cout << "0. Выйти из программы" << endl;
-	cin >> a;
-		switch (a)
-		{
-			int segnum;
-			way getmeth;
-			double res;
-			int ans;
-			double leflim, riglim;
-			math funk;
-		case 1:
-			cout << "1. Экспонента" << endl;
-			cout << "2. Синус" << endl;
-			cout << "3. Косинус" << endl;
-			cout << "4. Гиперболический синус" << endl;
-			cout << "5. Гиперболический косинус" << endl;
-			cout << "6. Квадратный корень" << endl;
-			cout << "7. Натуральный логарифм" << endl;
-			cin >> ans;
-			switch (ans)
-			{
-			case _exp:
-				funk = exp;
-				printf("exp");
-				break;
-			case _sin:
-				printf("sin");
-				funk = sin;
-				break;
-			case _cos:
-				printf("cos");
-				funk = cos;
-				break;
-			case _sinh:
-				printf("sinh");
-				funk = sinh;
-				break;
-			case _cosh:
-				printf("cosh");
-				funk = cosh;
-				break;
-			case _sqrt:
-				printf("sqrt");
-				funk = sqrt;
-				break;
-			case _log:
-				printf("log");
-				funk = log;
-				break;
-			default:
-				printf("default");
-				funk = abs;
-				break;
-			}
-			s.GetFoo(funk);
-			break;
-		case 2:
-			cin >> leflim >> riglim;
-			s.GetLim(leflim, riglim);
-			break;
-		case 3:
-			limits = s.OutLim();
-			cout << "" << limits[0] << endl;
-			cout << "" << limits[1] << endl;
-			break;
-		case 4:
-			cin >> segnum;
-			s.GetsegNum(segnum);
-			break;
-		case 5:
-			cin >> ans;
-			switch (ans)
-			{
-			case _left:
-				getmeth = _left;
-				break;
-			case _mid:
-				getmeth = _mid;
-				break;
-			case _right:
-				getmeth = _right;
-				break;
-			default:
-				break;
-			}
-			s.GetWay(getmeth);
-			break;
-		case 6:
-			s.calc();
-			break;
-		case 7:
-			res = s.out();
-			break;
-		default:
-			cout << "Всего доброго, до свидания!" << endl;
-			break;
-		}
-	}while (a);
+	cout << "Программа по вычислению интеграла от элементарных функций методом прямоугольников"<<endl;
+	menu();
 	system("pause");
 }
-//void integral::left_rectangle()
-//{
-//	double addend;
-//	double segment = (b - a) / (double)n;
-//	double a1 = a;
-//	double b1 = a + segment;
-//	for (int i = 0; i < n; i++)
-//	{
-//		addend = foo(a1) * (b1 - a1);
-//		res += addend;
-//		a1 += segment;
-//		b1 += segment;
-//	}
-//}
-//void integral::right_rectangle()
-//{
-//	double addend;
-//	double segment = (b - a) / (double)n;
-//	double a1 = a;
-//	double b1 = a + segment;
-//	for (int i = 1; i < n + 1; i++)
-//	{
-//		addend = foo(b1) * (b1 - a1);
-//		res += addend;
-//		a1 += segment;
-//		b1 += segment;
-//	}
-//}
-//void integral::mid_rectangle()
-//{
-//	double addend;
-//	double segment = (b - a) / (double)n;
-//	double a1 = a;
-//	double b1 = a + segment;
-//	for (int i = 0; i < n; i++)
-//	{
-//		addend = foo(midfinder(a1, b1)) * (b1 - a1);
-//		res += addend;
-//		a1 += segment;
-//		b1 += segment;
-//	}
-//}
-
