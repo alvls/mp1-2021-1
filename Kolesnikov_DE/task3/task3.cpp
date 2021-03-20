33d30eba2638a0c5e46929ffaa9e4a41831f212d
kde37777 <kolessnikoff2016@yandex.ru>
2021-03-20 14:55:07 +0300

    task3 2-nd build

--- Kolesnikov_DE/task3/task3.cpp
+++ Kolesnikov_DE/task3/task3.cpp
@@ -1,33 +1,41 @@
-include <fstream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iostream>
 using namespace std;
 class Dict
 {
-    public:
+    private:
         struct Dictationary
         {
-            string *word;
-            string *tword;
-        }
-        int k;
-    private:
-        Dict()
+            string word;
+            string tword;
+        };
+        int len;
+        int store;
+        Dictationary *mass;
+    public:
+        Dict(): len(100)
         {
-            k = 0;
             mass = new Dictationary [100];
+        }
+        Dict(int _len) : len(_len)
+        {
+            mass = new Dictationary [len];
+        }
+        Dict(const Dict& other) : len(other.len) store(other.store)
+        {
+            delete [] mass;
+            Dictationary* mass = new Dictationary [len];
+            for(int i = 0;i < len;i++)
+            {
+                mass[i].word = other.word;
+                mass[i].tword = other.tword;
+            }
         }
         ~Dict()
         {
-            for(int i = 0;i < k;i++){
-                delete [] mass[i].word;
-                delete [] mass[i].tword;
-                delete [] mass;
-            }
-        }
-        Dict(char *wd)
-        {
-            k = 1;
-            mass = new Dictationary [100];
-            mass[0].word = new string [strlen(wd)];
+            delete [] mass;
         }
         string Get_Tr(string wd)
         {
@@ -36,62 +44,67 @@ class Dict
                     return mass[i].tword;
                 }
             }
-            return -1
+            return 1;
         }
         int Len_Dict()
         {
             return k;
         }
         int Is_There()
-       {
+        {
             for(int i = 0;i < k;i++){
                 if (mass[i].word == wd){return 0;}
             }
-            return -1;
+            return 1;
                 
         }
         int Set_Word(string w,string tw)
         {
-            if(k+1==max){return -1}
-            mass[k+1] = new string[stren(w)];
-            mass[k+1] = new string[stren(tw)];
+            if(k+1==len){return -1;}
+            mass[k+1].word = w;
+            mass[k+1].tword = tw;
             k++;
         }
-        string Ch_Tr(string twd)
+        int Ch_Tr(string wd,string twd)
         {
             for(int i = 0;i < k;k++){
                 if (mass[i].word == wd){
-                    delete [] mass[i].tword;
-                    mass[i].tword = new string[strlen(twd)];
-                    return -1;
+                    mass[i].tword = twd;
+                    return 0;
                 }
             }
-            return -1;
+            return 1;
         }
         string To_File()
         {
             ofstream fout;
             fout.open("Dict.txt");
-            fout << Dictationary << endl;
+            if(!fout){return 1;}
             for(int i = 0;i < k;i++){
-                fout << mass[i].word<<mass[i].tword<<endl;
+                fout << mass[i].word << ' ' << mass[i].tword << endl;
             }
             fout.close();
             return Dict.txt;
         }
-        int ReadFrFile(string path)
+        int Read_Fr_File(string path)
         {
             string buf;
-            string tbuf;
             ifstream fin(path);
-            if(!fin.is_open()){return -1;};
-            for(int i = 0;i < k;i++){
-                delete [] mass[i].word;
-                delete [] mass[i].tword;
+            if(!fin){return -1;};
+            else{
                 delete [] mass;
+                int nlen;
+                for(;!fin.getline(fin);nlen++){
+                }
+                len = nlen;
+                store = nlen;
+                mass = new Dictationary [len];
+                for(int i = 0;!fin.eof();i++){
+                    if(i % 2 == 0){fin >> mass[i/2].word;}  
+                    else{fin >> mass[i/2].tword;}
+                return 0;
+                }
             }
-            mass = new Dict [100];
-            fin >> buf>>tbuf; //read op
+            
         } 
-
 };
